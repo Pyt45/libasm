8e7dc4bd5659ef4954679027464715eda8c8f568
aaqlzim <ayoub1234aqlzim@gmail.com>
2020-11-02 14:37:46 +0100

    add_strdup

---
+++ ft_strdup.s
@@ -0,0 +1,36 @@
+        global          _ft_strdup
+        section         .text
+        extern          _malloc
+        extern          _ft_strcpy
+        extern          _ft_strlen
+
+; _ft_strdup:
+;         call            _ft_strlen
+;         mov             r8, rax
+;         add             r8, 1
+;         call            _malloc
+;         jmp             dup
+
+; increment:
+;        inc             rcx
+
+; dup:
+;        xor             rcx, rcx
+;        mov             dl, byte[rdi + rcx]
+;        mov             byte[rax + rcx], dl
+;        cmp             byte[rdi + rcx], 0
+;        jne             increment
+
+; return:
+;        mov             [rax], qword rdi 
+;        ret
+
+_ft_strdup:
+        call            _ft_strlen
+        push            rdi
+        mov             rdi, rax
+        call            _malloc
+        pop             rcx
+        mov             rdi, rcx
+        call            _ft_strcpy
+        ret
