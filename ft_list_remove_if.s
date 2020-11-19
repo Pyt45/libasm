0bcde9c4c48c069b74ea39a6122a71ae58a34704
aaqlzim <ayoub1234aqlzim@gmail.com>
2020-11-19 14:14:32 +0100

    add_atoi_base

--- ft_list_remove_if.s
+++ ft_list_remove_if.s
@@ -125,3 +125,92 @@ delone:
 
 end:
 				ret
+
+;         global			_ft_list_remove_if
+;         section			.text
+;     	extern			_free
+
+; _ft_list_remove_if:
+; 		push			r12 ; r12 = current = *begin ; r9 = last
+; 		push			rbx ; rbx = tmp = current
+; 		cmp				rdi, 0 ; !begin
+; 		je				end
+; 		mov				r12, [rdi] ; r12 = *begin
+; 		cmp				rsi, 0
+; 		je				end
+; 		cmp				rdx, 0
+; 		je				end
+; 		xor				rbx, rbx ; tmp = rbx = NULL
+; 		xor				r9, r9 ; last = r9 = NULL
+; 		jmp				big_loop
+		
+
+; free_elt:
+; 		mov				rbx, [rdi] ; tmp = current
+; 		mov				rcx, [rdi]
+; 		mov				r8, [rcx + 8]
+; 		mov				[rdi], r8 ; current = current->next
+; 		push			rdi
+; 		push			rsi
+; 		push			rdx
+
+; 		mov				rdi, rbx
+; 		call			_free
+
+; 		pop				rdx
+; 		pop				rsi
+; 		pop				rdi
+; 		jmp				big_loop
+
+
+; give_it_add:
+; 		mov				rcx, [rdi] ; rcx = current
+; 		mov				rax, [rcx + 8] ; rax = current->next
+; 		mov				r12, rax ; *begin = current->next
+; 		jmp				free_elt
+
+; remove_elt:
+; 		mov				rcx, r12 	; rcx = *begin
+; 		mov				r8, [rdi] 	; r8 = current
+; 		sub				r8, rcx
+; 		cmp				r8, 0 		; current == *begin
+; 		je				give_it_add
+; 		mov				rcx, [rdi] ; rcx = current
+; 		mov				rax, [rcx + 8] ; rax = current->next
+; 		mov				[r9 + 8], rax ; last->next = current->next
+; 		jmp				free_elt
+
+; big_loop:
+; 		cmp				qword [rdi], 0 ; !(current) ; current = *begin
+; 		je				end
+; 		mov				rcx, [rdi] ; rcx = *begin
+; 		;mov				r9, rsi
+; 		push			rdi
+; 		push			rsi
+; 		push			rdx
+; 		push			r9
+
+; 		mov				rdi, [rcx] ; rdi = current->dara
+; 		;mov				rsi, r9
+; 		call			rdx ; cmp(current->data, rsi = data_ref)
+		
+; 		pop				r9
+; 		pop				rdx
+; 		pop				rsi
+; 		pop				rdi
+; 		cmp				rax, 0
+; 		jne				increment
+; 		jmp				remove_elt
+
+; increment:
+; 		mov				r9, [rdi] ; last = current
+; 		mov				r8, [rdi]
+; 		mov				rcx, [r8 + 8]
+; 		mov				[rdi], rcx ; current = current->next
+; 		jmp				big_loop
+
+; end:
+; 		mov				[rdi], r12
+; 		pop				rbx
+; 		pop				r12
+; 		ret
--- libasm_42/test.c
+++ libasm_42/test.c
@@ -1,75 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct	s_list
+int		ft_isspace(int c)
 {
-    void			*data;
-    struct s_list	*next;
-}				t_list;
+	return (c == ' ' || c == '\t' || c == '\v'
+	|| c == '\n' || c == '\f' || c == '\r');
+}
 
-int		ft_list_size1(t_list *begin_list)
+int		ft_strchar(char *str, char find)
 {
+	char	*ptr;
+
+	ptr = str;
+	while (*ptr)
+		if (find == *ptr++)
+			return ((int)(ptr - str - 1));
+	return (-1);
+}
+
+int		check_base(char *base)
+{
+	char	*base_check;
 	int		len;
-	t_list	*tmp;
+	int		i;
 
-	len = 0;
-	tmp = begin_list;
-	while (tmp)
+	base_check = base;
+	while (*base_check++)
+		;
+	if ((len = (int)(base_check - base - 1)) <= 1)
+		return (0);
+	i = -1;
+	while (++i < len)
 	{
-		len++;
-		tmp = tmp->next;
+		if (base[i] == '+' || base[i] == '-')
+			return (0);
+		if (base[i] <= 32 || base[i] > 126)
+			return (0);
+		if (ft_strchar(base + i + 1, base[i]) >= 0)
+			return (0);
 	}
 	return (len);
 }
 
-void	ft_list_push_front(t_list **begin_list, void *data);
-int		ft_list_size(t_list *list);
-//void	ft_list_sort(t_list **begin_list, int (*cmp)());
-//void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)());
-
-int		cmp(void *s1, void *s2)
+int		ft_atoi_base1(char *str, char *base)
 {
-	size_t			i;
-	unsigned char	*b1;
-	unsigned char	*b2;
+	int		base_len;
+	int		index;
+	char	*ptr;
+	int		num;
+	int		sign;
 
-	i = 0;
-	b1 = (unsigned char*)s1;
-	b2 = (unsigned char*)s2;
-	while (b1[i] || b2[i])
-	{
-		if (b1[i] != b2[i])
-			return (b1[i] - b2[i]);
-		i++;
-	}
-	return (0);
+	num = 0;
+	base_len = strlen(base);
+	if (!check_base(base))
+		return (0);
+	while (ft_isspace(*str))
+		str++;
+	sign = (*str == '-') ? -1 : 1;
+	str += (str[0] == '+' || sign < 0) ? 1 : 0;
+	// while (find_char_base(base, *str) < base_len)
+	// 	num = (num * base_len) - find_char_base(base, *str++);
+	ptr = str;
+	while (*ptr)
+		if (ft_strchar(base, *ptr++) < 0 && *(ptr - 1) != '-' && *(ptr - 1) != '+')
+			return (0);
+	while ((index = ft_strchar(base, *str++)) >= 0)
+		num = num * base_len + index;
+	return (sign * num);
 }
 
 int     main()
 {
-	t_list *list1;
-	t_list *list2;
-	t_list *list3;
-	t_list *list4;
-
-	list1 = malloc(sizeof(t_list));
-	list2 = malloc(sizeof(t_list));
-	list3 = malloc(sizeof(t_list));
-	list4 = malloc(sizeof(t_list));
-
-	list1->data = "helloss";
-	list2->data = "aaa";
-	list3->data = "bye";
-	list4->data = "new";
-	
-	list1->next = list2;
-	list2->next = list3;
-	list3->next = list4;
-	list4->next = NULL;
-
-
-    
-	printf("Before :%s\n", list1->data);
-	ft_list_push_front(&list1, list4);
-	printf("After :%s\n", list1->data);
+	return 0;
 }
